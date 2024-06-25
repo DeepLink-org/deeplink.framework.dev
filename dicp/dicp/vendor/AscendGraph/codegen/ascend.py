fix paged_attention precision.

dicp/dicp/vendor/AscendGraph/codegen/ascend.py
@@ -238,16 +238,6 @@ class AscendCodegen(torch.fx.Interpreter):
                     if not torch.allclose(a, b, atol=atol, rtol=rtol, equal_nan=True):
                         import pdb;pdb.set_trace()
                         pass
-                
-                def dump_tensors(args):
-                    dir_path = '/tzy/dynamic_shape/lightllm'
-                    new_args = list(args)
-                    for i, x in enumerate(new_args):
-                        if not isinstance(x, int):
-                            x = x.cpu()
-                        new_args[i] = x
-                    with open(dir_path + '/args.pkl', 'wb') as f:
-                        pickle.dump(new_args, f)
             """, strip=True
         )
         return self.import_code.getvalue()
@@ -267,7 +257,7 @@ class AscendCodegen(torch.fx.Interpreter):
         # dynamic shape feature
         if len(self.sym_in_args) > 0 or len(self.sym_to_inputs) > 0:
             args = ['_' if arg not in shape_symint and arg not in self.sym_to_inputs.values() else arg for arg in self.args]
-            call_body.writeline(f"dump_tensors(args)")
+            # call_body.writeline(f"dump_tensors(args)")
             call_body.writeline(f"({','.join(args)}) = args")
 
             # assign SymInt to InputArgs relationship
@@ -1626,13 +1616,14 @@ class AscendOverrides:
         return op.to_node()
 
     @staticmethod
-    def PromptFlashAttention(name, q, k, v, head_num, seqlen, mask, head_dim):
+    def PromptFlashAttention(name, q, k, v, head_num, seqlen, mask, head_dim, numKeyValueHeads):
         op = OP(name, "PromptFlashAttention")
         op.set_input("query", q)
         op.set_input("key", k)
         op.set_input("value", v)
         op.set_input("atten_mask", mask)
         op.set_attr_int("num_heads", head_num)
+        op.set_attr_int("num_key_value_heads", numKeyValueHeads)
         op.set_attr_float("scale_value", float(1 / math.sqrt(head_dim)))
         op.set_attr_str("input_layout", "BSH")
         return op.to_node()
@@ -1644,7 +1635,6 @@ class AscendOverrides:
         op.set_dynamic_input("key", kv_input_num, k_list)
         op.set_dynamic_input("value", kv_input_num, v_list)
         op.set_attr_int("num_heads", head_num)
-        
         # op.set_attr_float("scale_value", float(1 / math.sqrt(dim)))
         op.set_attr_int("num_key_value_heads", kv_head_num)
         op.set_attr_str("input_layout", input_layout)

dicp/dicp/vendor/AscendGraph/codegen/load_and_run.py
@@ -116,7 +116,7 @@ class MemoryPool:
             free, _, ret = acl.rt.get_mem_info(ACL_HBM_MEM)
             check_ret("acl.rt.get_mem_info", ret)
             
-            self.work_size = int(6 * 1024 * 1024 * 1024)
+            self.work_size = int(1 * 1024 * 1024 * 1024)
             self.work_tensor = torch.empty(
                 self.work_size, dtype=torch.bool, device=dipu_device_str)
             self.work_ptr = self.work_tensor.data_ptr()

dicp/dicp/vendor/AscendGraph/conversion.py
@@ -968,7 +968,6 @@ class AtenToAscendTransformer(SingleOpTransformer):
                     tensor_unsqueeze_len = none_count_in_indices - i if contiguous_flag \
                         else none_count_in_indices
                 indice_i_shape = index.node.meta['val'].shape
-                assert not not_all_num_shape(indice_i_shape)
                 tensor_reshape_shape.append(list(indice_i_shape) + [1] * tensor_unsqueeze_len)
         assert first_tensor_pos != -1, "all elements of indices is None, unsupported"
         tensor_broadcast_shape = list(torch.broadcast_shapes(*tensor_reshape_shape))
@@ -997,7 +996,7 @@ class AtenToAscendTransformer(SingleOpTransformer):
 
         # stack(pack) all index
         target_indices_broadcast_shape = list(torch.broadcast_shapes(*(none_reshape_shape + tensor_reshape_shape)))
-        target_broadcast_shape_proxy = self.get_const_proxy(target_indices_broadcast_shape, torch.int32)
+        target_broadcast_shape_proxy = self.get_shape_proxy(target_indices_broadcast_shape, torch.int32)
         stack_input_list = []
         const_int32_0 = self.get_const_proxy(0, torch.int32, target_shape=[])
         const_int32_1 = self.get_const_proxy(1, torch.int32, target_shape=[])
@@ -1014,7 +1013,7 @@ class AtenToAscendTransformer(SingleOpTransformer):
                 to_be_reshape_proxy = index
                 index_reshape_shape = tensor_reshape_shape[tensor_idx]
                 tensor_idx += 1
-            reshape_shape_proxy = self.get_const_proxy(index_reshape_shape, torch.int32)
+            reshape_shape_proxy = self.get_shape_proxy(index_reshape_shape, torch.int32)
             reshape_proxy = self.get_proxy(ascend_op.Reshape, (to_be_reshape_proxy, reshape_shape_proxy))
             stack_input_list.append(self.get_proxy(ascend_op.BroadcastTo, (reshape_proxy, target_broadcast_shape_proxy)))
         return self.get_proxy(ascend_op.Pack, (stack_input_list, len(target_indices_broadcast_shape))), \
@@ -1067,7 +1066,7 @@ class AtenToAscendTransformer(SingleOpTransformer):
         stacked_indices, indices_broadcast_shape, stacked_indices_last_dim = \
             self.compute_stacked_indices(indices, x.node.meta['val'].shape)
         values_broadcast_shape = indices_broadcast_shape + x_shape[stacked_indices_last_dim:]  # batch_shape + inner_shape
-        values_broadcast_shape_op = self.get_const_proxy(values_broadcast_shape, torch.int32)
+        values_broadcast_shape_op = self.get_shape_proxy(values_broadcast_shape, torch.int32)
         broadcasted_values = self.get_proxy(ascend_op.BroadcastTo, (values, values_broadcast_shape_op))
         return self.get_proxy(ascend_op.TensorScatterUpdate, (x, stacked_indices, broadcasted_values))
 
@@ -1742,11 +1741,13 @@ class AtenToAscendTransformer(SingleOpTransformer):
         return self.get_proxy(ascend_op.Identity, (out, 0))
 
     @register_conversion(torch.ops.lightllm.prompt_attention_inference.default)
-    def prompt_attention_inference(self, q, k, v, seqlen, num_head, head_dim):
+    def prompt_attention_inference(self, q, k, v, seqlen, num_head, head_dim, numKeyValueHeads):
         if isinstance(num_head, torch.fx.proxy.Proxy):
             num_head = int(sympy.N(num_head.node.meta['val']))
         if isinstance(head_dim, torch.fx.proxy.Proxy):
             head_dim = int(sympy.N(head_dim.node.meta['val']))
+        if isinstance(numKeyValueHeads, torch.fx.proxy.Proxy):
+            numKeyValueHeads = int(sympy.N(numKeyValueHeads.node.meta['val']))
         q_shape = list(q.node.meta['val'].shape)
         seq_len = q_shape[1]
         shape = [seq_len, seq_len]
@@ -1756,7 +1757,7 @@ class AtenToAscendTransformer(SingleOpTransformer):
         mask = self.get_proxy(ascend_op.Tril, (mask,))
         mask = self.get_proxy(ascend_op.LogicalNot, (mask,))
         q = self.get_proxy(ascend_op.Cast, (q, get_ascend_dtype(torch.float16)))
-        fa = self.get_proxy(ascend_op.PromptFlashAttention, (q, k, v, num_head, seqlen, mask, head_dim))
+        fa = self.get_proxy(ascend_op.PromptFlashAttention, (q, k, v, num_head, seqlen, mask, head_dim, numKeyValueHeads))
         return fa
 
     def incre_flash_attention(self, q, k, v, head_num, kv_head_num, dim):

dicp/dicp/vendor/AscendGraph/ext_ops.py
@@ -50,17 +50,17 @@ def lightllm_rms_norm_impl(x, weight, eps):
 
 
 @torch._custom_op.impl.custom_op('lightllm::prompt_attention_inference')
-def prompt_attention_inference(q: Tensor, k: Tensor, v: Tensor, seqlen: Tensor, num_head: int, head_dim: int) -> Tensor:
+def prompt_attention_inference(q: Tensor, k: Tensor, v: Tensor, seqlen: Tensor, num_head: int, head_dim: int, numKeyValueHeads: int) -> Tensor:
     ...
 
 
 @prompt_attention_inference.impl_abstract()
-def lightllm_prompt_attention_inference_abstract(q: Tensor, k: Tensor, v: Tensor, seqlen: Tensor, num_head: int, head_dim: int):
+def lightllm_prompt_attention_inference_abstract(q: Tensor, k: Tensor, v: Tensor, seqlen: Tensor, num_head: int, head_dim: int, numKeyValueHeads: int):
     return torch.empty_like(q)
 
 
 @prompt_attention_inference.impl(['cpu', 'cuda'])
-def lightllm_prompt_attention_inference_impl(q, k, v, seqlen, num_head, head_dim):
+def lightllm_prompt_attention_inference_impl(q, k, v, seqlen, num_head, head_dim, numKeyValueHeads: int):
     assert q.shape[0] == 1, "prompt attention just support bs=1 for now."
     bs = q.shape[0]
     seqlen = seqlen.item()