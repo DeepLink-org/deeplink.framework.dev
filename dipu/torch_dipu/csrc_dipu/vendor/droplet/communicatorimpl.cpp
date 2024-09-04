6ee0c9b15f315520e8eb9c7ea95fd2caa53e725d
Gong-air <994321849@qq.com>
2024-09-04 15:28:23 +0800

    test load

--- dipu/torch_dipu/csrc_dipu/vendor/droplet/communicatorimpl.cpp
+++ dipu/torch_dipu/csrc_dipu/vendor/droplet/communicatorimpl.cpp
@@ -6,9 +6,7 @@
 
 #include "csrc_dipu/runtime/device/basedef.h"
 #include "csrc_dipu/runtime/devproxy/deviceproxy.h"
-#ifdef USE_PCCL
-#include <pccl.h>
-#endif  // USE_PCCL
+#include "pccl.h"
 #include <torch/csrc/distributed/c10d/Types.hpp>
 
 #include <csrc_dipu/common.h>
@@ -18,7 +16,7 @@ namespace dipu {
 
 namespace devapis {
 
-#ifdef USE_PCCL
+// #ifdef USE_PCCL
 
 #define LINE_HELPER1(x) #x
 #define LINE_HELPER2(x) LINE_HELPER1(x)
@@ -86,7 +84,8 @@ static const std::map<at::ScalarType, pcclDataType_t> toPcclDataType = {
 
 DIPU_API diclResult_t diclGetCommAsyncError(diclComm_t comm) {
   pcclResult_t pcclAsyncErr;
-  CALL_PCCL(pcclCommGetAsyncError(comm, &pcclAsyncErr));
+  // CALL_PCCL(pcclCommGetAsyncError(comm, &pcclAsyncErr));
+  DIPU_CALL_PCCL(pcclCommGetAsyncError, comm, &pcclAsyncErr);
   // shuold we return pcclInProgress as success or not?
   if (pcclAsyncErr != pcclSuccess) {
     return DICL_ERR_UNDEF;
@@ -96,19 +95,22 @@ DIPU_API diclResult_t diclGetCommAsyncError(diclComm_t comm) {
 }
 
 DIPU_API diclResult_t diclGetUniqueId(commUniqueId* uniqueId) {
-  CALL_PCCL(pcclGetUniqueId(uniqueId));
+  // CALL_PCCL(pcclGetUniqueId(uniqueId));
+  DIPU_CALL_PCCL(pcclGetUniqueId, uniqueId);
   return DICL_SUCCESS;
 }
 
 DIPU_API diclResult_t diclCommInitRank(diclComm_t* comm, int nranks,
                                        commUniqueId uniqueId, int rank,
                                        int localDeviceId) {
-  CALL_PCCL(pcclCommInitRank(comm, nranks, uniqueId, rank));
+  // CALL_PCCL(pcclCommInitRank(comm, nranks, uniqueId, rank));
+  DIPU_CALL_PCCL(pcclCommInitRank, comm, nranks, uniqueId, rank);
   return DICL_SUCCESS;
 }
 
 DIPU_API diclResult_t diclCommDestroy(diclComm_t comm) {
-  CALL_PCCL(pcclCommDestroy(comm));
+  // CALL_PCCL(pcclCommDestroy(comm));
+  DIPU_CALL_PCCL(pcclCommDestroy, comm);
   return DICL_SUCCESS;
 }
 
@@ -118,8 +120,10 @@ DIPU_API diclResult_t diclAllReduce(const void* sendbuff, void* recvbuff,
                                     deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
   RedOpTypeToPcclRedOp_t(reduceOp, pcclReduceOp);
-  CALL_PCCL(pcclAllReduce(sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
-                          comm, stream));
+  // CALL_PCCL(pcclAllReduce(sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
+  //                         comm, stream));
+  DIPU_CALL_PCCL(pcclAllReduce, sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
+                 comm, stream);
   return DICL_SUCCESS;
 }
 
@@ -128,8 +132,10 @@ DIPU_API diclResult_t diclBroadcast(const void* sendbuff, void* recvbuff,
                                     int root, diclComm_t comm,
                                     deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
-  CALL_PCCL(pcclBroadcast(sendbuff, recvbuff, count, pcclDataType, root, comm,
-                          stream));
+  // CALL_PCCL(pcclBroadcast(sendbuff, recvbuff, count, pcclDataType, root, comm,
+  //                         stream));
+  DIPU_CALL_PCCL(pcclBroadcast, sendbuff, recvbuff, count, pcclDataType, root, comm,
+                 stream);
   return DICL_SUCCESS;
 }
 
@@ -137,7 +143,8 @@ DIPU_API diclResult_t diclAllGather(const void* sendBuf, void* recvBuf,
                                     size_t count, at::ScalarType datatype,
                                     diclComm_t comm, deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
-  CALL_PCCL(pcclAllGather(sendBuf, recvBuf, count, pcclDataType, comm, stream));
+  // CALL_PCCL(pcclAllGather(sendBuf, recvBuf, count, pcclDataType, comm, stream));
+  DIPU_CALL_PCCL(pcclAllGather, sendBuf, recvBuf, count, pcclDataType, comm, stream);
   return DICL_SUCCESS;
 }
 
@@ -147,8 +154,10 @@ DIPU_API diclResult_t diclReduce(const void* sendbuff, void* recvbuff,
                                  diclComm_t comm, deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
   RedOpTypeToPcclRedOp_t(reduceOp, pcclReduceOp);
-  CALL_PCCL(pcclReduce(sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
-                       root, comm, stream));
+  // CALL_PCCL(pcclReduce(sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
+  //                      root, comm, stream));
+  DIPU_CALL_PCCL(pcclReduce, sendbuff, recvbuff, count, pcclDataType, pcclReduceOp,
+                 root, comm, stream);
   return DICL_SUCCESS;
 }
 
@@ -159,6 +168,8 @@ DIPU_API diclResult_t diclReduceScatter(
   RedOpTypeToPcclRedOp_t(reduceOp, pcclReduceOp);
   CALL_PCCL(pcclReduceScatter(sendBuf, recvBuf, recvCount, pcclDataType,
                               pcclReduceOp, comm, stream));
+  DIPU_CALL_PCCL(pcclReduceScatter, sendBuf, recvBuf, recvCount, pcclDataType,
+                 pcclReduceOp, comm, stream);
   return DICL_SUCCESS;
 }
 
@@ -166,7 +177,8 @@ DIPU_API diclResult_t diclSend(const void* sendbuff, size_t count,
                                at::ScalarType datatype, int peer,
                                diclComm_t comm, deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
-  CALL_PCCL(pcclSend(sendbuff, count, pcclDataType, peer, comm, stream));
+  // CALL_PCCL(pcclSend(sendbuff, count, pcclDataType, peer, comm, stream));
+  DIPU_CALL_PCCL(pcclSend, count, pcclDataType, peer, comm, stream);
   return DICL_SUCCESS;
 }
 
@@ -175,143 +187,144 @@ DIPU_API diclResult_t diclRecv(void* recvbuff, size_t count,
                                diclComm_t comm, deviceStream_t stream) {
   ScalarTypeToPcclDataType_t(datatype, pcclDataType);
   CALL_PCCL(pcclRecv(recvbuff, count, pcclDataType, peer, comm, stream));
+  DIPU_CALL_PCCL(pcclRecv, recvbuff, count, pcclDataType, peer, comm, stream);
   return DICL_SUCCESS;
 }
 
-#else  // USE_PCCL
-
-namespace {
-
-using diclCommValue_t = std::remove_pointer_t<diclComm_t>;
-constexpr diclCommValue_t kMagicComm = 0x5043434C;  // "PCCL"
-
-diclComm_t createDiclComm() { return new diclCommValue_t(kMagicComm); }
-
-void destroyDiclComm(diclComm_t comm) { delete comm; }
-
-void checkCommOrThrow(diclComm_t comm) {
-  if (comm == nullptr || *comm != kMagicComm) {
-    throw std::runtime_error("Invalid comm.");
-  }
-}
-
-[[noreturn]] void throwNotSupportedError() {
-  throw std::runtime_error(
-      "PCCL is not enabled. DIPU only allows single GPU communication.");
-}
-
-void checkNrankOrThrow(int nranks) {
-  if (nranks != 1) {
-    throwNotSupportedError();
-  }
-}
-
-void checkRankOrThrow(int rank) {
-  if (rank != 0) {
-    throwNotSupportedError();
-  }
-}
-
-void singleDeviceMemcpy(deviceStream_t stream, void* dst, const void* src,
-                        size_t nbytes) {
-  auto device = devproxy::current_device();
-  devproxy::memCopyD2DAsync(stream, nbytes, device, dst, device, src);
-}
-
-}  // namespace
-
-const int DICL_UNIQUE_ID_BYTES_SIZE = 0;
-
-DIPU_API diclResult_t diclGetCommAsyncError(diclComm_t comm) {
-  checkCommOrThrow(comm);
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclGetUniqueId(commUniqueId* uniqueId) {
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclCommInitRank(diclComm_t* comm, int nranks,
-                                       commUniqueId uniqueId, int rank,
-                                       int localDeviceId) {
-  checkNrankOrThrow(nranks);
-  checkRankOrThrow(rank);
-  DIPU_LOGW(
-      "PCCL is not enabled. DIPU will simulate single GPU "
-      "communication using memcpy.");
-  *comm = createDiclComm();
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclCommDestroy(diclComm_t comm) {
-  checkCommOrThrow(comm);
-  destroyDiclComm(comm);
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclAllReduce(const void* sendbuff, void* recvbuff,
-                                    size_t count, at::ScalarType datatype,
-                                    const ReduceOp& reduceOp, diclComm_t comm,
-                                    deviceStream_t stream) {
-  checkCommOrThrow(comm);
-  singleDeviceMemcpy(stream, recvbuff, sendbuff,
-                     count * at::elementSize(datatype));
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclBroadcast(const void* sendbuff, void* recvbuff,
-                                    size_t count, at::ScalarType datatype,
-                                    int root, diclComm_t comm,
-                                    deviceStream_t stream) {
-  checkCommOrThrow(comm);
-  singleDeviceMemcpy(stream, recvbuff, sendbuff,
-                     count * at::elementSize(datatype));
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclAllGather(const void* sendBuf, void* recvBuf,
-                                    size_t count, at::ScalarType datatype,
-                                    diclComm_t comm, deviceStream_t stream) {
-  checkCommOrThrow(comm);
-  singleDeviceMemcpy(stream, recvBuf, sendBuf,
-                     count * at::elementSize(datatype));
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclReduce(const void* sendbuff, void* recvbuff,
-                                 size_t count, at::ScalarType datatype,
-                                 const ReduceOp& reduceOp, int root,
-                                 diclComm_t comm, deviceStream_t stream) {
-  checkCommOrThrow(comm);
-  checkRankOrThrow(root);
-  singleDeviceMemcpy(stream, recvbuff, sendbuff,
-                     count * at::elementSize(datatype));
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclReduceScatter(
-    void* sendBuf, void* recvBuf, size_t recvCount, at::ScalarType datatype,
-    const ReduceOp& reduceOp, diclComm_t comm, deviceStream_t stream) {
-  singleDeviceMemcpy(stream, recvBuf, sendBuf,
-                     recvCount * at::elementSize(datatype));
-  return DICL_SUCCESS;
-}
-
-DIPU_API diclResult_t diclSend(const void* sendbuff, size_t count,
-                               at::ScalarType datatype, int peer,
-                               diclComm_t comm, deviceStream_t stream) {
-  throwNotSupportedError();
-  return DICL_ERR_UNDEF;
-}
-
-DIPU_API diclResult_t diclRecv(void* recvbuff, size_t count,
-                               at::ScalarType datatype, int peer,
-                               diclComm_t comm, deviceStream_t stream) {
-  throwNotSupportedError();
-  return DICL_ERR_UNDEF;
-}
-
-#endif  // USE_PCCL
+// #else  // USE_PCCL
+//
+// namespace {
+//
+// using diclCommValue_t = std::remove_pointer_t<diclComm_t>;
+// constexpr diclCommValue_t kMagicComm = 0x5043434C;  // "PCCL"
+//
+// diclComm_t createDiclComm() { return new diclCommValue_t(kMagicComm); }
+//
+// void destroyDiclComm(diclComm_t comm) { delete comm; }
+//
+// void checkCommOrThrow(diclComm_t comm) {
+//   if (comm == nullptr || *comm != kMagicComm) {
+//     throw std::runtime_error("Invalid comm.");
+//   }
+// }
+//
+// [[noreturn]] void throwNotSupportedError() {
+//   throw std::runtime_error(
+//       "PCCL is not enabled. DIPU only allows single GPU communication.");
+// }
+//
+// void checkNrankOrThrow(int nranks) {
+//   if (nranks != 1) {
+//     throwNotSupportedError();
+//   }
+// }
+//
+// void checkRankOrThrow(int rank) {
+//   if (rank != 0) {
+//     throwNotSupportedError();
+//   }
+// }
+//
+// void singleDeviceMemcpy(deviceStream_t stream, void* dst, const void* src,
+//                         size_t nbytes) {
+//   auto device = devproxy::current_device();
+//   devproxy::memCopyD2DAsync(stream, nbytes, device, dst, device, src);
+// }
+//
+// }  // namespace
+//
+// const int DICL_UNIQUE_ID_BYTES_SIZE = 0;
+//
+// DIPU_API diclResult_t diclGetCommAsyncError(diclComm_t comm) {
+//   checkCommOrThrow(comm);
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclGetUniqueId(commUniqueId* uniqueId) {
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclCommInitRank(diclComm_t* comm, int nranks,
+//                                        commUniqueId uniqueId, int rank,
+//                                        int localDeviceId) {
+//   checkNrankOrThrow(nranks);
+//   checkRankOrThrow(rank);
+//   DIPU_LOGW(
+//       "PCCL is not enabled. DIPU will simulate single GPU "
+//       "communication using memcpy.");
+//   *comm = createDiclComm();
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclCommDestroy(diclComm_t comm) {
+//   checkCommOrThrow(comm);
+//   destroyDiclComm(comm);
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclAllReduce(const void* sendbuff, void* recvbuff,
+//                                     size_t count, at::ScalarType datatype,
+//                                     const ReduceOp& reduceOp, diclComm_t comm,
+//                                     deviceStream_t stream) {
+//   checkCommOrThrow(comm);
+//   singleDeviceMemcpy(stream, recvbuff, sendbuff,
+//                      count * at::elementSize(datatype));
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclBroadcast(const void* sendbuff, void* recvbuff,
+//                                     size_t count, at::ScalarType datatype,
+//                                     int root, diclComm_t comm,
+//                                     deviceStream_t stream) {
+//   checkCommOrThrow(comm);
+//   singleDeviceMemcpy(stream, recvbuff, sendbuff,
+//                      count * at::elementSize(datatype));
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclAllGather(const void* sendBuf, void* recvBuf,
+//                                     size_t count, at::ScalarType datatype,
+//                                     diclComm_t comm, deviceStream_t stream) {
+//   checkCommOrThrow(comm);
+//   singleDeviceMemcpy(stream, recvBuf, sendBuf,
+//                      count * at::elementSize(datatype));
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclReduce(const void* sendbuff, void* recvbuff,
+//                                  size_t count, at::ScalarType datatype,
+//                                  const ReduceOp& reduceOp, int root,
+//                                  diclComm_t comm, deviceStream_t stream) {
+//   checkCommOrThrow(comm);
+//   checkRankOrThrow(root);
+//   singleDeviceMemcpy(stream, recvbuff, sendbuff,
+//                      count * at::elementSize(datatype));
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclReduceScatter(
+//     void* sendBuf, void* recvBuf, size_t recvCount, at::ScalarType datatype,
+//     const ReduceOp& reduceOp, diclComm_t comm, deviceStream_t stream) {
+//   singleDeviceMemcpy(stream, recvBuf, sendBuf,
+//                      recvCount * at::elementSize(datatype));
+//   return DICL_SUCCESS;
+// }
+//
+// DIPU_API diclResult_t diclSend(const void* sendbuff, size_t count,
+//                                at::ScalarType datatype, int peer,
+//                                diclComm_t comm, deviceStream_t stream) {
+//   throwNotSupportedError();
+//   return DICL_ERR_UNDEF;
+// }
+//
+// DIPU_API diclResult_t diclRecv(void* recvbuff, size_t count,
+//                                at::ScalarType datatype, int peer,
+//                                diclComm_t comm, deviceStream_t stream) {
+//   throwNotSupportedError();
+//   return DICL_ERR_UNDEF;
+// }
+//
+// #endif  // USE_PCCL
 
 }  // end namespace devapis
 
